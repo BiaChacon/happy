b50a689205545db4a622f9262e692904b9bfb933
biachacon <anabheatrizchacon@outlook.com>
2020-10-14 09:31:21 -0300

    destination uploads

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default {
   storage: multer.diskStorage({
-    destination: path.join(__dirname, '.', '..', 'uploads'),
+    destination: path.join(__dirname, '.', '..', '..', 'uploads'),
     filename: (request, file, cb) => {
       const fileName = `${Date.now()}-${file.originalname}`;
 
